fbc9e3f757f3da6c26752459b8a1b66d316b9669
Kommerla <bharatchandrakommerla01.cb@gmail.com>
2021-07-05 21:48:40 +0530

    Sixth Commit

--- correlation.py
+++ correlation.py
@@ -2,7 +2,16 @@
 import json
 
 def load_journal(json):
-    json = journal.json
     f = open(json)
     correlation = json.load(f)
     return correlation
+
+def compute_phi(correlation,carrot):
+    row = 0
+    n11,n00,n10,n01 = 0,0,0,0
+    n1x,n0x,nx1,nx0 = 0,0,0,0
+    f = open(json)
+    correlation = json.load(f)
+    for d in correlation:
+        if event in d['events']:
+            n1x +=1
