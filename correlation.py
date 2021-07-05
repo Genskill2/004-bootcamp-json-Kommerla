4d93059debd1b48f42a17123a7071b88708b9cd1
Kommerla <bharatchandrakommerla01.cb@gmail.com>
2021-07-05 18:21:41 +0530

    Fifth Commit

--- correlation.py
+++ correlation.py
@@ -1,7 +1,7 @@
 # Add the functions in this file
 import json
 
-def load_journal(journal):
+def load_journal(json):
     f = open(journal.json)
     correlation = json.loads(f)
     return correlation
