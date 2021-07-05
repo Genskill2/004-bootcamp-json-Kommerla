7783bc46d1b38200fd664c9b53088c7c84bb3c59
Kommerla <bharatchandrakommerla01.cb@gmail.com>
2021-07-05 18:19:55 +0530

    Fifth Coomit-5

--- correlation.py
+++ correlation.py
@@ -1,10 +1,7 @@
 # Add the functions in this file
 import json
 
-def load_journal(journal.json):
+def load_journal(journal):
     f = open(journal.json)
     correlation = json.loads(f)
     return correlation
-def compute_phi():
-    m_1 = correlation['carrot']
-    m_2 = correlation['squirrel']
