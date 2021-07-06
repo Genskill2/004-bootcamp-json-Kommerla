4e4bbb8f7ee4e8bb1ed4c2c821b4c78c30a8f97d
Kommerla <bharatchandrakommerla01.cb@gmail.com>
2021-07-06 12:58:32 +0530

    compute_phi - 7

--- correlation.py
+++ correlation.py
@@ -13,26 +13,26 @@ def compute_phi(correlation):
 	b = 'carrot'
 	row = 0
 	n11,n00,n10,n01 = 0,0,0,0
-	n1x,n0x,nx1,nx0 = 0,0,0,0
+	n1_,n0_,n_1,n_0 = 0,0,0,0
 	f = open(json)
 	correlation = json.load(f)
 	for c in correlation:
 		if a in c:
-			n1x += 1
+			n1_ += 1
 			n10 += 1
-			nx0 += 1
+			n_0 += 1
 			if b in c:
 				n11 += 1
-				n1x += 1
+				n1_ += 1
 		if b in c:
-			nx1 += 1
+			n_1 += 1
 			n01 += 1
-			n0x += 1
+			n0_ += 1
 		else:
 			n00 += 1
-			nx0 += 1
-			n0x += 1
+			n_0 += 1
+			n0_ += 1
 
-		return n11,n00,n10,n01,n1x,n0x,nx1,nx0
-	 row = (n11*n00 - n10*n01)/sqrt(n1x * n0x * nx1 * nx0)
+		return n11,n00,n10,n01,n1_,n0_,n_1,n_0
+	 row = (n11*n00 - n10*n01)/sqrt(n1_ * n0_ * n_1 * n_0)
 	 return row
