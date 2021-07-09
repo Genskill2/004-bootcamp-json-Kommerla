c13cf86053e36f763e090b3fc378f61612b167a3
Kommerla <bharatchandrakommerla01.cb@gmail.com>
2021-07-09 22:35:04 +0530

    Adds all functions

--- correlation.py
+++ correlation.py
@@ -3,8 +3,8 @@ import json
 import math
 from math import sqrt
 
-def load_journal(jsonfile):
-	f = open(jsonfile)
+def load_journal(json):
+	f = open(json)
 	correlation = json.load(f)
 	return correlation
 
@@ -17,22 +17,36 @@ def compute_phi(correlation):
 	f = open(json)
 	correlation = json.load(f)
 	for c in correlation:
-		if a in c:
-			n1_ += 1
-			n10 += 1
-			n_0 += 1
-			if b in c:
-				n11 += 1
-				n1_ += 1
-		if b in c:
-			n_1 += 1
-			n01 += 1
-			n0_ += 1
-		else:
-			n00 += 1
-			n_0 += 1
-			n0_ += 1
+	    if a in c:
+	        n1_ += 1
+	        n10 += 1
+	        n_0 += 1
+	    elif a and b in c:
+	        n11 += 1
+	        n1_ += 1
+	    elif b in c:
+	        n_1 += 1
+	        n01 += 1
+	        n0_ += 1
+	    else:
+	        n00 += 1
+	        n_0 += 1
+	        n0_ += 1
+	    return n11, n00, n10, n01, n1_, n0_, n_1, n_0
+        row = (n11 * n00 - n10 * n01) / sqrt(n1_ * n0_ * n_1 * n_0)
+	    return row
 
-		return n11,n00,n10,n01,n1_,n0_,n_1,n_0
-	 row = (n11*n00 - n10*n01)/sqrt(n1_ * n0_ * n_1 * n_0)
-	 return row
+def compute_correlations(json):
+	correlation = load_journal(json)
+	e = {}
+	for d in data:
+		for event in d['event']:
+			if e not in e.keys():
+				e[event] = comput_phi(json)
+	return e
+
+def diagnose(json):
+	e = compute_correlations(json)
+	high_positive_correlate = max(e,key = e.get)
+	high_negative_correlate = min(e,key = e.get)
+	return high_positive_correlate,high_negative_correlate
